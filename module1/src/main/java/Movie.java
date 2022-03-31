e942719f420019a2f0a68de62b97c84c7a1f33fa
valerieJJ <820439600@qq.com>
2022-03-31 14:35:25 +0800

    v-2

--- module1/src/main/java/Movie.java
+++ module1/src/main/java/Movie.java
@@ -3,6 +3,7 @@ import java.io.Serializable;
 public class Movie implements Serializable {
     String mid = "001";
     String name = "smooth criminal";
+    String describ = "";
 
     public Movie() {
     }
