2e03dace1615ff1a237f567a447fc0c93d5acaa8
mike <820439600@qq.com>
2022-04-26 21:33:08 +0800

    vjj

---
+++ web/src/main/java/vjj/web/controller/HelloController.java
@@ -0,0 +1,16 @@
+package vjj.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "welcome";
+    }
+}
